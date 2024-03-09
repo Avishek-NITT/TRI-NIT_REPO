48754b583a0f040911380c2403a93de484c27296
Sharvstar <162751187+Sharvstar@users.noreply.github.com>
2024-03-09 12:14:38 +0530

    Update index.js

--- Frontend/reactjs/backend/index.js
+++ Frontend/reactjs/backend/index.js
@@ -37,7 +37,8 @@ const app = express();
 const mongoDB = require("./db");
 mongoDB();
 
-// app.use("/api/")
+app.use(express.json());
+app.use("/api", require('./Routes/CreateUser'))
 
 app.listen(port , (error)=>{
     if(error){
