25474b051164bca0660bb3d12ee844d93a8df567
Sharvstar <162751187+Sharvstar@users.noreply.github.com>
2024-03-09 12:51:43 +0530

    Update CreateUser.js

--- Frontend/reactjs/backend/Routes/CreateUser.js
+++ Frontend/reactjs/backend/Routes/CreateUser.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const User = require("../models/User");
+const { body, validationResult } = require('express-validator');
 
-router.post('/createUser', async(req,res)=>{
+router.post('/createUser', 
+[body('email').isEmail(),
+body('name').isLength({min : 4}),
+body('password' , 'Incorrect password').isLength({min : 5})],
+async(req,res)=>{
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors : errors.array()});
+    }
+    
     try{
         await User.create({
-            name:"Tanush",
-            password:"00495856",
-            email:"tanushsomisetty@gmail.com",
-            location:"nhjdjhk",
+            name:req.body.name,
+            password:req.body.password,
+            email:req.body.email,
+            location:req.body.location,
         })
         res.send({success : 1});
     } catch(err){
